87e86c18845423c375245f6385fb7b4e026a9767
gtovar1985 <gtovar@incaztech.com>
2013-11-12 15:27:47 -0600

    Change name file load_mexico_spec to load_countries_spec

--- spec/lib/load_mexico_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Load Mexico" do
-  it "doesn't raise any error" do
-    expect {
-      SpreeLatinGeonames::Engine.load_country("mexico")
-    }.to_not raise_error
-  end
-end
